41b17f9f3423ce54b39d8ff9d630f2fa8e8ebe45
Orelsaada <54078278+Orelsaada@users.noreply.github.com>
2019-09-22 01:30:11 +0300

    Headless mode

--- Main.py
+++ Main.py
@@ -1,96 +1,97 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common import exceptions
 
-driver = webdriver.Chrome("ENTER WEBDRIVER PATH HERE")  <---- Enter the path of chrome webdriver.
 
-driver.get(url="https://www.subpals.com")
+channel_id = input('Enter your channel id: ')
+channel_pass = input('Enter your channel password: ')
+my_email = input('Enter your email: ')
+email_pass = input('Enter your email password: ')
+
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome("F:\Python\Projects\ChromeWebdriver\chromedriver.exe",options=chrome_options)
+
+driver.get(url="https://www.subpals.com/")
 driver.implicitly_wait(10)
 
- #Canceling the ad
-time.sleep(10)
-driver.find_element(By.ID,'image-f2c4f9b9f6bd33e7629a866a5dbb5955').click() 
 
-driver.find_element(By.LINK_TEXT,'Login / Register').click()
+login_register = driver.find_element(By.XPATH,'//*[@id="mega-menu-item-32613"]/a')
+driver.execute_script("arguments[0].click();", login_register)
+
 
 driver.switch_to.frame(1)
 
-#channel id
-driver.find_element(By.NAME, 'channelid').send_keys('ENTER HERE YOUR CHANNEL ID')    <---- Enter Channel ID.
+driver.find_element(By.NAME, 'channelid').send_keys(channel_id)  # channel id
 
 login_button = driver.find_element(By.NAME, 'submit')
 driver.execute_script("arguments[0].click();", login_button)
 
-#Channel password
-driver.find_element_by_name('password').send_keys('ENTER HERE YOUR CHANNEL PASSWORD')   <---- Enter your channel password
+driver.find_element_by_name('password').send_keys(channel_pass)  # Channel password
 
-#Logging to SubPals
 login_button = driver.find_element(By.NAME, 'submit')
-driver.execute_script("arguments[0].click();", login_button)     
+driver.execute_script("arguments[0].click();", login_button)     # LOGIN SUBPALS
+
+driver.refresh()
 
 driver.switch_to.frame('iFrameResizer0')
-
-#Activate the plan
 try:
-    driver.find_element_by_xpath('/html/body/div/center[2]/div/div[1]/div[2]/form/div/a').click() 
+    driver.find_element_by_xpath('/html/body/div/center[2]/div/div[1]/div[2]/form/div/a').click() #activate plan
 except:
     pass
 time.sleep(5)
+try:
+    left_videos = int(driver.find_element_by_id('remainingHint').text)
+    print(f'[+] {left_videos} videos [+]')
 
-#20 times of likes and sub
-for i in range(21):
-    if i == 0:
+    while left_videos:
         try:
             driver.switch_to.frame("iFrameResizer0")
         except:
             pass
-
-        driver.find_element_by_id('likeSub2').click()
-
+        first_button = driver.find_element_by_id('likeSub2')
+        driver.execute_script("arguments[0].click();", first_button)
         driver.switch_to.window(driver.window_handles[1])
 
-        #Login to Youtube:
-        driver.find_element(By.XPATH,'//*[@id="buttons"]/ytd-button-renderer/a').click()
+        try:
+            # Login to Youtube:
+            youtube_login = driver.find_element(By.XPATH,'//*[@id="buttons"]/ytd-button-renderer/a')
+            driver.execute_script("arguments[0].click();", youtube_login)
+            driver.find_element_by_id('Email').send_keys(my_email)
+            next_button = driver.find_element(By.ID, 'next')
+            driver.execute_script("arguments[0].click();", next_button)
 
-        driver.find_element(By.ID, 'identifierId').send_keys('ENTER HERE YOUR EMAIL') <---- Enter here your email
-        driver.find_element(By.ID, 'identifierNext').click()
-
-        driver.find_element(By.NAME, 'password').send_keys('ENTER HERE YOUR EMAIL PASSWORD') <---- Enter here your email password
-        pass_next_button = driver.find_element(By.ID, 'passwordNext')
-        driver.execute_script("arguments[0].click();", pass_next_button)
-
-        #Like and Sub:
-        like_button = driver.find_element_by_xpath('//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a').click()
-        sub_button = driver.find_element_by_xpath('//*[@id="subscribe-button"]/ytd-subscribe-button-renderer/paper-button').click()
-
-        driver.switch_to.window(driver.window_handles[0])
-
-        driver.switch_to.frame("iFrameResizer0")
-        time.sleep(7)
-        driver.find_element_by_id('likeSub3').click()
-        time.sleep(5)
-
-    else:
-        driver.refresh()
-        time.sleep(5)
-        driver.switch_to.frame("iFrameResizer0")
-        driver.find_element_by_id('likeSub2').click()
-
-        driver.switch_to.window(driver.window_handles[1])
+            driver.find_element(By.ID, 'Passwd').send_keys(email_pass)
+            pass_next_button = driver.find_element(By.ID, 'signIn')
+            driver.execute_script("arguments[0].click();", pass_next_button)
+            print("Logined to youtube")
+        except:
+            pass
 
         # Like and Sub:
-        like_button = driver.find_element_by_xpath(
-            '//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a').click()
-        sub_button = driver.find_element_by_xpath(
-            '//*[@id="subscribe-button"]/ytd-subscribe-button-renderer/paper-button').click()
-
+        like_button = driver.find_element_by_xpath('//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a').click()
+        sub_button = driver.find_element_by_xpath('//*[@id="subscribe-button"]/ytd-subscribe-button-renderer/paper-button').click()
         driver.switch_to.window(driver.window_handles[0])
-
-        time.sleep(10)
-
         driver.switch_to.frame("iFrameResizer0")
-        driver.find_element_by_id('likeSub3').click()
-        time.sleep(6)
+        time.sleep(7)
 
+        confirm_button = driver.find_element_by_id('likeSub3')
+        driver.execute_script("arguments[0].click();", confirm_button)
+
+        WebDriverWait(driver, 50).until(
+            ec.invisibility_of_element_located((By.ID, 'likeSub3')))
+        try:
+            left_videos = int(driver.find_element_by_id('remainingHint').text)
+            print(f'[+] {left_videos} videos [+]')
+        except:
+            print("No more videos left on SubPals.")
+            break
+        time.sleep(2)
+
+except exceptions.NoSuchElementException:
+    print("You already used it in the last 24 hours.")
